bd47eca1931194ee785ae5fc4c5389c60000e6c1
Diego-Guarise <l.guariselopez@gmail.com>
2022-05-05 21:33:53 -0300

    T.T

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,28 +7,28 @@ Log parsing
 import sys
 
 
-lines = 0
+i = 0
 size = 0
-status = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-          '404': 0, '405': 0, '500': 0}
+status = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 input = ['200', '301', '400', '401', '403', '404', '405', '500']
-
 try:
     for line in sys.stdin:
-        lines += 1
-        if len(line.split(' ')) > 2:
-            size += int(line.split(' ')[-1])
-            if line.split(' ')[-2] in status:
-                status[line.split(' ')[-2]] += 1
-        if lines % 10 == 0:
-            print('File size: {}'.format(size))
+        i += 1
+        sp = line.split(' ')
+        if len(sp) > 2:
+            size += int(sp[-1])
+            if sp[-2] in status:
+                status[sp[-2]] += 1
+        if i % 10 == 0:
+            print("File size: {}".format(size))
             for tmp in input:
-                if status[tmp] != 0:
-                    print('{}: {}'.format(tmp, status[tmp]))
+                if status[tmp]:
+                    print("{}: {}".format(tmp, status[tmp]))
 except KeyboardInterrupt:
     pass
 finally:
-    print('File size: {}'.format(size))
-    for tmp in input:
-        if status[tmp] != 0:
-            print('{}: {}'.format(tmp, status[tmp]))
+    print("File size: {}".format(size))
+    for tmp2 in input:
+        if status[tmp2]:
+            print("{}: {}".format(tmp2, status[tmp2]))
